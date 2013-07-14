a3b99dabd40d713111deb6b29ea5f3f57ca3f99f
Alex Clark <aclark@aclark.net>
2013-07-14 09:14:21 -0400

    Add fields

--- collective_project/content/client.py
+++ collective_project/content/client.py
@@ -1,6 +1,14 @@
 from plone.directives import form
+from zope.schema import TextLine
+from zope.schema import Text
 
 
 class IClient(form.Schema):
     """
     """
+
+    title = TextLine(title="Name")
+    email = TextLine(title="E-mail", required=False)
+    website = TextLine(title="Website", required=False)
+    address = Text(title="Address", required=False)
+    description = Text(title="Notes", required=False)
